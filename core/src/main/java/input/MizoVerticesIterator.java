95e761163c3c62d6371dea9f459efcb43b7a5076
Imri Hecht <imriqwe@gmail.com>
2016-12-10 00:16:31 +0200

    MizoBaseRelationsIterator is a base class for an iterator that parses MizoVertex and MizoEdge objects with their properties, given a IMizoRelationParser object MizoEdgesIterator is an iterator that parses vertices and edges, but returns instances of MizoEdge MizoVerticesIterator is an iterator that parses vertices and edges, but returns instances of MizoVertex

---
+++ core/src/main/java/input/MizoVerticesIterator.java
@@ -0,0 +1,53 @@
+package input;
+
+import com.google.common.collect.Iterators;
+import com.google.common.collect.PeekingIterator;
+import models.MizoEdge;
+import models.MizoVertex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+
+/**
+ * Created by imrihecht on 12/10/16.
+ */
+public class MizoVerticesIterator extends  MizoBaseRelationsIterator<MizoVertex> {
+    private static final Logger log = LoggerFactory.getLogger(MizoVerticesIterator.class);
+
+    /**
+     * Each call to this iterator returns a relation (vertex property or in/out edge) to be parsed
+     */
+    protected PeekingIterator<IMizoRelationParser> relationsIterator;
+
+    public MizoVerticesIterator(Iterator<IMizoRelationParser> relationsIterator, IMizoRDDConfig config) {
+        super(config);
+
+        this.relationsIterator = Iterators.peekingIterator(relationsIterator);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return relationsIterator.hasNext();
+    }
+
+    @Override
+    public MizoVertex next() {
+        long lastVertexId = this.relationsIterator.peek().getVertexId();
+        MizoVertex currentVertex = setupNewVertex(this.relationsIterator.peek());
+
+        while (this.relationsIterator.hasNext() && lastVertexId == this.relationsIterator.peek().getVertexId()) {
+            MizoEdge edgeOrNull = getEdgeOrNull(this.relationsIterator.next(), currentVertex);
+
+            if (edgeOrNull != null) {
+                if (edgeOrNull.isOutEdge()) {
+                    currentVertex.initializedOutEdges().add(edgeOrNull);
+                } else {
+                    currentVertex.initializedInEdges().add(edgeOrNull);
+                }
+            }
+        }
+
+        return currentVertex;
+    }
+}
